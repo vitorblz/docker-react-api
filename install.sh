25e5e89ac5be50d1fd9956694ec09150009c7264
Vitor Carvalho <vitorblz@gmail.com>
2018-12-05 13:59:18 -0200

    Improvements in install

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+npm install --prefix ./node-server/;
+npm install --prefix ./frontend/;
+cp node-server/.env.model node-server/.env;
+cp .env.model .env;
+echo "Now run: docker-composer up";
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-npm install --prefix ./node-server/;
-npm install --prefix ./frontend/;
-docker-compose up;
